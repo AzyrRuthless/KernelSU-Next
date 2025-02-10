3de49bb79fdd4f8c224de6b240dd47f9208f68bc
AzyrRuthless <132790292+AzyrRuthless@users.noreply.github.com>
2025-02-10 18:43:45 +0700

    Kernel: add support for KSU Offcial, KSU Rissu, & KSU backslashxx

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -333,5 +333,8 @@ bool ksu_is_manager_apk(char *path)
 		return false;
 	}
 
-	return check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH);
+	return check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH) ||
+	       check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)         ||
+	       check_v2_signature(path, EXPECTED_XX_SIZE, EXPECTED_XX_HASH)  ||
+	       check_v2_signature(path, EXPECTED_RISSU_SIZE, EXPECTED_RISSU_HASH);
 }
