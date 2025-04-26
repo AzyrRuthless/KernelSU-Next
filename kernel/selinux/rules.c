99d3a9813a1f6ca09fd24222b4c990a2360a0f7b
AzyrRuthless <132790292+AzyrRuthless@users.noreply.github.com>
2025-04-26 17:36:28 +0700

    selinux: Add custom allow rules for vendor_init and hal_power

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -139,6 +139,17 @@ void apply_kernelsu_rules()
 	ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "getpgid");
 	ksu_allow(db, "system_server", KERNEL_SU_DOMAIN, "process", "sigkill");
 
+	// Custom rule: allow vendor_init proc_sched:file w_file_perms;
+	// (Granting all file permissions for simplicity using ALL)
+	ksu_allow(db, "vendor_init", "proc_sched", "file", ALL);
+
+	// Custom rule: allow hal_power_default { proc proc_sched }:{ file lnk_file } rw_file_perms;
+	// (Granting all file/lnk_file permissions for simplicity using ALL)
+	ksu_allow(db, "hal_power_default", "proc", "file", ALL);
+	ksu_allow(db, "hal_power_default", "proc", "lnk_file", ALL);
+	ksu_allow(db, "hal_power_default", "proc_sched", "file", ALL);
+	ksu_allow(db, "hal_power_default", "proc_sched", "lnk_file", ALL);
+
 	mutex_unlock(&ksu_rules);
 }
 
