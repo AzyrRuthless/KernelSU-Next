ff66634fadc54e0874827d35e839908fe2a9ada6
Azyr Ruthless <azyrruthless@tutamail.com>
2025-09-11 12:40:30 +0700

    selinux: Add custom allow rules for Graphics Allocator HAL & appdomain system_server

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -150,6 +150,15 @@ void apply_kernelsu_rules()
 	ksu_allow(db, "hal_power_default", "proc_sched", "file", ALL);
 	ksu_allow(db, "hal_power_default", "proc_sched", "lnk_file", ALL);
 
+	// Custom rules for Graphics Allocator HAL
+	// allow servicemanager hal_graphics_allocator_default:service_manager { find add };
+	ksu_allow(db, "servicemanager", "hal_graphics_allocator_default", "service_manager", "find");
+	ksu_allow(db, "servicemanager", "hal_graphics_allocator_default", "service_manager", "add");
+
+	// allow { appdomain system_server } hal_graphics_allocator_service:service_manager find;
+	ksu_allow(db, "appdomain", "hal_graphics_allocator_service", "service_manager", "find");
+	ksu_allow(db, "system_server", "hal_graphics_allocator_service", "service_manager", "find");
+
 	mutex_unlock(&ksu_rules);
 }
 
